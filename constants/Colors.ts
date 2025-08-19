6648fd95d594d5acaf2e2447f4daa97112ed1e23
Nguyen Le Ngoc Huy <71955356+ngochuy2001-neo@users.noreply.github.com>
2025-08-19 21:14:39 +0700

    Init project

---
+++ constants/Colors.ts
@@ -0,0 +1,15 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined for a single theme.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColor = '#0a7ea4';
+
+export const Colors = {
+  text: '#11181C',
+  background: '#fff',
+  tint: tintColor,
+  icon: '#687076',
+  tabIconDefault: '#687076',
+  tabIconSelected: tintColor,
+};
---
+++ hooks/useColorScheme.web.ts
@@ -0,0 +1,7 @@
+
+/**
+ * To support static rendering, this value needs to be re-calculated on the client side for web
+ */
+export function useColorScheme() {
+  return 'light';
+}
